7257afe2fe1d416b05de10c5bed9921670ab0865
Vasco Barnabé <vascobarnabe2000@gmail.com>
2021-12-03 00:17:58 +0000

    Add files via upload

---
+++ Sistemas Móveis e Aplicações/main/java/pt/sma/ivebeenthere/MapActivity.kt
@@ -0,0 +1,11 @@
+package pt.sma.ivebeenthere
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+}
---
+++ Sistemas Móveis e Aplicações/main/java/pt/sma/ivebeenthere/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package pt.sma.ivebeenthere.ui.login
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+import pt.sma.ivebeenthere.data.LoginDataSource
+import pt.sma.ivebeenthere.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                loginRepository = LoginRepository(
+                    dataSource = LoginDataSource()
+                )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
